398c5fb57ac5e154cd22b5546ec72a361dab92a4
Patrik Dvořáček <dvoracek.patrik@gmail.com>
2020-03-16 18:08:20 +0100

    Setup basic structure

--- coronaFighters.js
+++ coronaFighters.js
@@ -1,7 +1,36 @@
 'use strict';
 
-function initGame() {
+class Game {
 
+}
+
+class Player {
+
+    constructor(name, world) {
+        this.score = 0;
+        this.health = 100;
+        this.name = name;
+        this.world = world;
+    }
+}
+
+class Virus {
+    
+    constructor(sprite) {
+        this.sprite = sprite;
+    }
+}
+
+class World {
+
+    constructor(map) {
+        this.map = map;
+    }
+}
+
+function initGame() {
+    var gameArea = document.getElementById('gameArea');
+    gameArea.innerText = '';
 }
 
 initGame();
